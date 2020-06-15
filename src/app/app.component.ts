8bd3a26a209b65cd68e1178e4a15a34fccef8cd3
l08084 <l08084.1989@gmail.com>
2020-06-15 23:23:10 +0900

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'array-bug';
+  public array: number[] = [];
+
+  constructor() {
+    this.addArray();
+    this.addArray();
+    this.addArray();
+    console.log(this.array);
+    // [1, 2, 3]ではなく、[empty, 1, empty, 3, empty, 5]と表示されてしまう
+  }
+
+  /**
+   * 配列に1〜呼び出された回数の数字を代入する
+   *
+   * @memberof AppComponent
+   */
+  public addArray() {
+    let item = ++this.array.length;
+    this.array.push(item);
+  }
 }
